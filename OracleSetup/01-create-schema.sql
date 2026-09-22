6460b0ccadef91abddb51e4faca8af4f7607fd76
Pierre <nop77svk@gmail.com>
2026-09-22 15:01:15 +0200

    ➕ DB setup scripts

---
+++ OracleSetup/01-create-schema.sql
@@ -0,0 +1,19 @@
+﻿create user benchmark
+identified by benchmark
+default tablespace users
+quota unlimited on users
+temporary tablespace temp
+;
+
+----------------------------------------------------------------------------------------------------
+
+grant create session, create table to benchmark;
+
+drop table if exists benchmark.t_test_data purge;
+
+create table benchmark.t_test_data
+(
+    id                  integer not null primary key
+)
+organization index
+;
