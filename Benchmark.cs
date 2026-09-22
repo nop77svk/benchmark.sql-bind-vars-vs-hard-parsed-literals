bfad0c6ae724fa1759e905bb9d73e8fa6c9a3293
Pierre <nop77svk@gmail.com>
2026-09-22 15:01:02 +0200

    ➕ solution init

---
+++ Benchmark.cs
@@ -0,0 +1,10 @@
+﻿namespace BenchmarkBindVarsAndHardcodesInOracle;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet;
+
+public class Benchmark
+{
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal static class Program
+{
+    private const string OracleDockerImageUri = @"container-registry.oracle.com/database/free:latest";
+    private const string OracleDockerSysPassword = @"abc";
+    private const string OracleDockerDatabaseCharset = @"AL32UTF8";
+
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Initialising");
+        Testcontainers.Oracle.OracleBuilder builder = new Testcontainers.Oracle.OracleBuilder(OracleDockerImageUri);
+        Testcontainers.Oracle.OracleContainer container = builder
+            .WithAutoRemove(true)
+            .WithCleanUp(true)
+            .WithPortBinding(hostPort: 1529, containerPort: 1521)
+            .WithEnvironment(@"ORACLE_PWD", OracleDockerSysPassword)
+            .WithEnvironment(@"ORACLE_CHARACTERSET", OracleDockerDatabaseCharset)
+            .WithEnvironment(@"ENABLE_ARCHIVELOG", @"false")
+            .WithEnvironment(@"ENABLE_FORCE_LOGGING", @"false")
+            .Build();
+
+        try
+        {
+            Console.WriteLine("Starting Oracle DB");
+            await container.StartAsync();
+
+            Console.WriteLine("Press any key 😁 to finish");
+            Console.ReadKey();
+        }
+        finally
+        {
+            await container.StopAsync();
+        }
+    }
+}
